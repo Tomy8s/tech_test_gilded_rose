f87021577ad08d9eb35da4ca9c0fc93110d54ca0
Tomy8s <tomy8s@hotmail.com>
2016-11-30 13:25:39 +0000

    added tests for quality constraints before sell-by

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,13 +2,13 @@ require 'gilded_rose'
 
 describe GildedRose do
   let (:items) { [
-    Item.new(name="+5 Dexterity Vest", sell_in=2, quality=25),
-    Item.new(name="Elixir of the Mongoose", sell_in=2, quality=25),
-    Item.new(name="Aged Brie", sell_in=2, quality=25),
-    Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=2, quality=25),
-    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=25),
-    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=25),
-    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=25),
+    Item.new(name="+5 Dexterity Vest", sell_in=5, quality=3),
+    Item.new(name="Elixir of the Mongoose", sell_in=5, quality=47),
+    Item.new(name="Aged Brie", sell_in=5, quality=47),
+    Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=5, quality=25),
+    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=47),
+    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=43),
+    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=40),
     # This Conjured item does not work properly yet
     Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
   ] }
@@ -27,11 +27,21 @@ describe GildedRose do
       it "reduces quality by 1" do
         expect{ subject.update_quality }.to change{ items[0].quality }.by(-1)
       end
+
+      it "does not decrease quality bellow 0" do
+        4.times{ subject.update_quality }
+        expect(items[0].quality).to eq 0
+      end
     end
 
     context "when Aged Brie ages([2])" do
       it "increases quality by 1" do
         expect{ subject.update_quality }.to change{ items[2].quality }.by(+1)
+      end
+
+      it "does not increase quality over 50" do
+        4.times{ subject.update_quality }
+        expect(items[2].quality).to eq 50
       end
     end
 
@@ -45,17 +55,32 @@ describe GildedRose do
       it "increases quality by 1" do
         expect{ subject.update_quality }.to change{ items[4].quality }.by(+1)
       end
+
+      it "does not increase quality over 50" do
+        4.times{ subject.update_quality }
+        expect(items[4].quality).to eq 50
+      end
     end
 
     context "when backstage passes (10 days to concert) age([5])" do
       it "increases quality by 2" do
         expect{ subject.update_quality }.to change{ items[5].quality }.by(+2)
       end
+
+      it "does not increase quality over 50" do
+        4.times{ subject.update_quality }
+        expect(items[5].quality).to eq 50
+      end
     end
 
     context "when backstage passes (15 days to concert) age([4])" do
       it "increases quality by 3" do
         expect{ subject.update_quality }.to change{ items[6].quality }.by(+3)
+      end
+
+      it "does not increase quality over 50" do
+        4.times{ subject.update_quality }
+        expect(items[6].quality).to eq 50
       end
     end
 

914c14ac37ce0ef9a4b932f89bcaf857373762f2
Tomy8s <tomy8s@hotmail.com>
2016-11-30 12:31:08 +0000

    added items to spec

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,6 +1,10 @@
 require 'gilded_rose'
 
 describe GildedRose do
+  let(:vest) { Item.new(name="+5 Dexterity Vest", sell_in=15, quality=25) }
+  let(:brie) { Item.new(name="Aged Brie", sell_in=15, quality=25) }
+  let(:elixir) { Item.new(name="Elixir of the Mongoose", sell_in=15, quality=25) }
+  let(:sulfanas) { Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=15, quality=25) }
 
   describe "#update_quality" do
     it "does not change the name" do

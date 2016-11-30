6963f1f82a30bb286f4dbfa07fb40346f3ff97e1
Tomy8s <tomy8s@hotmail.com>
2016-11-30 13:11:50 +0000

    Basic tests for aging added

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,27 +1,65 @@
 require 'gilded_rose'
 
 describe GildedRose do
-  items = [
-    Item.new(name="+5 Dexterity Vest", sell_in=10, quality=20),
-    Item.new(name="Aged Brie", sell_in=2, quality=0),
-    Item.new(name="Elixir of the Mongoose", sell_in=5, quality=7),
-    Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80),
-    Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80),
-    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20),
-    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=49),
-    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49),
+  let (:items) { [
+    Item.new(name="+5 Dexterity Vest", sell_in=2, quality=25),
+    Item.new(name="Elixir of the Mongoose", sell_in=2, quality=25),
+    Item.new(name="Aged Brie", sell_in=2, quality=25),
+    Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=2, quality=25),
+    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=25),
+    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=25),
+    Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=25),
     # This Conjured item does not work properly yet
     Item.new(name="Conjured Mana Cake", sell_in=3, quality=6), # <-- :O
-  ]
+  ] }
 
   let(:subject) { GildedRose.new(items) }
 
-  context 'when any item ages' do
-    describe "#update_quality" do
+  describe "#update_quality" do
+    context "when any item ages" do
       it "does not change the name" do
         subject.update_quality
         expect(items[0].name).to eq "+5 Dexterity Vest"
       end
+    end
+
+    context "when unexeptional items age([0], [1])" do
+      it "reduces quality by 1" do
+        expect{ subject.update_quality }.to change{ items[0].quality }.by(-1)
+      end
+    end
+
+    context "when Aged Brie ages([2])" do
+      it "increases quality by 1" do
+        expect{ subject.update_quality }.to change{ items[2].quality }.by(+1)
+      end
+    end
+
+    context "when Sulfuras ages([3])" do
+      it "doesn't change quality" do
+        expect{ subject.update_quality }.to change{ items[3].quality }.by(0)
+      end
+    end
+
+    context "when backstage passes (15 days to concert) age([4])" do
+      it "increases quality by 1" do
+        expect{ subject.update_quality }.to change{ items[4].quality }.by(+1)
+      end
+    end
+
+    context "when backstage passes (10 days to concert) age([5])" do
+      it "increases quality by 2" do
+        expect{ subject.update_quality }.to change{ items[5].quality }.by(+2)
+      end
+    end
+
+    context "when backstage passes (15 days to concert) age([4])" do
+      it "increases quality by 3" do
+        expect{ subject.update_quality }.to change{ items[6].quality }.by(+3)
+      end
+    end
+
+    context "when conjured items age([8])" do
     end
   end
 

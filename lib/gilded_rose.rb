272e08ba7d13caac67f2a1bd01121db7d05fdd56
Tomy8s <tomy8s@hotmail.com>
2016-11-30 14:41:14 +0000

    implemented reduce_quality_of

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,7 +9,7 @@ class GildedRose
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            reduce_quality_of(item, 1)
           end
         end
       else
@@ -37,7 +37,7 @@ class GildedRose
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                reduce_quality_of(item, 1)
               end
             end
           else
